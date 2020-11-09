bb5945cb21010a4201e5944498419c7b07fa5f49
lucas ayala <lucas1569622193@gmail.com>
2020-11-09 18:16:43 -0300

    agregue mas datos a lucasenriquez

--- lucasenriquez.js
+++ lucasenriquez.js
@@ -2,7 +2,10 @@
 let lucas = {
     nombre : "Lucas",
     apellido : "Enriquez",
-    edad: 23
+    edad: 30,
+    ciudad : "valentin alsina",
+    pais : "argentina",
+    telefono : 1523768956, 
 }
 
 console.log(lucas)
